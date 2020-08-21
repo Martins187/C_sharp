d9c76d2730cb0141dcd3380a30313cc5e851fb1c
Martins187 <martins.balcuns1@gmail.com>
2020-08-21 14:16:15 +0300

    Fibonacci and dec to bin

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet, cik Fibonacci virknes skaitļus vēlaties lai izvada!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int y = 0;
+            int c = 0;
+            int z = 1;
+
+            for(int i = 0; i < a; i++)
+            {
+                Console.Write(y + " ");
+                c = y;
+                y = y + z;
+                z = c;
+            }
+        }
+    }
+}
